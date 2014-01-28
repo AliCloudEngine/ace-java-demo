103b92183304b165a2c1ca97671b7a22c27cd335
Jerry Lee <ding.lid@alibaba-inc.com>
2014-01-28 16:40:29 +0800

    first ci

---
+++ src/main/java/com/alibaba/ace/demo/model/User.java
@@ -0,0 +1,49 @@
+package com.alibaba.ace.demo.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 336719350483543743L;
+    private String name;
+    private boolean sex;
+    private byte age;
+    private int weight;
+
+    public String toString() {
+        return "name:" + name + ", sex:" + sex + ", age:" + age + ",weight:" + weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
---
+++ src/main/webapp/HelloAce.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+<link type="text/css" rel="stylesheet" href="css/demo.css" />
+<title>Hello ACE</title>
+</head>
+<body>
+		Hello ACE! My Name is <div class="appName"><%=config.getInitParameter("appName")%> </div>
+</body>
+</html>
