0c300a14bb01b22a07c3d1c36f3df0de08a325be
Jerry Lee <oldratlee@gmail.com>
2014-02-06 15:59:38 +0800

    add HelloAce servlet

---
+++ src/main/java/com/alibaba/ace/demo/HelloAce.java
@@ -0,0 +1,19 @@
+package com.alibaba.ace.demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author shuhong.dongsh
+ */
+public class HelloAce extends HttpServlet {
+
+    private static final long serialVersionUID = -1120930759438832012L;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Hello Ace!");
+    }
+}
