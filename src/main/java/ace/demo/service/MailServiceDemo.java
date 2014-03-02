8824c15cecb893b1f157f8d74393621427f10bac
Jerry Lee <oldratlee@gmail.com>
2014-03-02 15:20:32 +0800

    add mail service demo

---
+++ src/main/java/ace/demo/service/MailServiceDemo.java
@@ -0,0 +1,35 @@
+package ace.demo.service;
+
+import com.alibaba.appengine.api.mail.MailMessage;
+import com.alibaba.appengine.api.mail.MailService;
+import com.alibaba.appengine.api.mail.MailServiceFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Simple demo of {@link com.alibaba.appengine.api.cache.CacheService}
+ *
+ * @author ding.lid
+ */
+public class MailServiceDemo extends HttpServlet {
+    private static final long serialVersionUID = -2359366322435138488L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Begin mail service test.");
+
+        MailService mailService = MailServiceFactory.getMailService();
+        mailService.send(new MailMessage("foo@gmail.com", "oldratlee@gmail.com", "Hello", "From Mail Service"));
+
+        resp.getWriter().println("Finish mail service test.");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
